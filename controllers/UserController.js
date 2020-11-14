fdd3ad73b4f1cd5e620038845bbb92623df18edb
pesdesigner <pauloeduardo.dg@outlook.com>
2020-11-14 20:17:04 -0300

    Validação de formulário javascript
    Validando dados enviados pelo form

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -19,6 +19,8 @@ class UserController {
 
             let values = this.getValues()
 
+            if(!values) return false;
+
             this.getPhoto().then(
                 (content) => {
                     values.photo = content
@@ -68,11 +70,17 @@ class UserController {
     getValues() {
 
         let user = {}
+        let isValid = true
 
         var spread = [...this.formEl.elements]
-
         spread.forEach(function (field, index){
-      
+
+            if(['name', 'email', 'password',].indexOf(field.name) > -1 && !field.value){
+
+                field.parentElement.classList.add('has-error')
+                isValid = false
+            }
+
             if(field.name == 'gender'){
 
                 if(field.checked){ 
@@ -88,6 +96,10 @@ class UserController {
                 user[field.name] = field.value
             }
         })
+
+        if(!isValid){
+            return false
+        }
 
         return new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin )
     }
