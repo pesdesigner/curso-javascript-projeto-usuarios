8a672d29bb28913dbe37b03f260e329ff4bacd05
pesdesigner <pauloeduardo.dg@outlook.com>
2020-11-15 15:41:03 -0300

    Excluindo usuários
    Configurando botão para deletar usuário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -191,7 +191,7 @@ class UserController {
                 <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
                 </td>
             </tr>   
         `;
@@ -205,7 +205,15 @@ class UserController {
 
     addEventsTr(tr){
 
-        tr.querySelector('.btn-edit').addEventListener('click', e=>{
+        tr.querySelector('.btn-delete').addEventListener('click', e => { 
+            if(confirm("Deseja realmente excluir?")){
+                tr.remove()
+
+                this.updateCount()
+            }
+        })
+
+        tr.querySelector('.btn-edit').addEventListener('click', e => {
 
             let json = JSON.parse(tr.dataset.user)
 
